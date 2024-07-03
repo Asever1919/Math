461529de2c6ead3b6626a3184fbad6622b073c75
yukai <yukai@yukai.com>
2024-07-03 22:34:07 +0800

    修改prime内容

--- src/NumberATheorylgorithm/Prime.java
+++ src/NumberATheorylgorithm/Prime.java
@@ -2,21 +2,17 @@ package NumberATheorylgorithm;
 
 public class Prime {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 7;
-        boolean res = isPrime(num1, num2);
-        System.out.println(res);
+        int num = 5;
+        boolean res = isPrime(num);
+        System.out.println("prime:" + res);
     }
-    private static boolean isPrime(int num1, int num2) {
-        return gcd(num1, num2) == 1;
-    }
-    private static int gcd(int num1, int num2){
-//        while (num2 != 0){
-//            int temp = num2;
-//            num2 = num1 % num2;
-//            num1 = temp;
-//        }
-//        return num1;
-        return num2 == 0 ? num1 : gcd(num2, num1 % num2);
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
